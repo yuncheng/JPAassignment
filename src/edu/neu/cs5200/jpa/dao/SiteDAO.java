48b5a1d18e0b0a73a57c79e0d8d2cc280434289c
yuncheng <macbook@macbookdemacbook-pro.local>
2015-04-14 17:02:16 -0400

    Completed Q4

--- src/edu/neu/cs5200/jpa/dao/SiteDAO.java
+++ src/edu/neu/cs5200/jpa/dao/SiteDAO.java
@@ -1,12 +1,15 @@
 package edu.neu.cs5200.jpa.dao;
 
-import java.util.List;
+import java.util.*;
 
 import javax.persistence.*;
 
-import edu.neu.cs5200.jpa.models.Site;
-import edu.neu.cs5200.jpa.models.Tower;
+import edu.neu.cs5200.jpa.models.*;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/site")
 public class SiteDAO {
 
 	
@@ -16,60 +19,92 @@ public class SiteDAO {
 	//crud
 	
 	//createSite
-	public Site createSite(Site site) {
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Site> createSite(Site site) {
 		em.getTransaction().begin();
 		em.persist(site);
 		em.getTransaction().commit();
-		return site;
+		return findAllSites();
 	}
 	
-	//readSiteById
-	public Site findSite(Integer id) {
+	//findSite
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)	
+	public Site findSite(@PathParam("id") Integer id) {
 		return em.find(Site.class, id);
 	}
-	//readAllSites
+	
+	//findAllSites
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Site> findAllSites() {
 		Query query = em.createQuery("select site from Site site");
 		return (List<Site>) query.getResultList();
 	}
+	
 	//updateSite
-	public List<Site> updateSite(int siteId, Site site) {
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Site> updateSite(@PathParam("id") int siteId, Site site) {
 		em.getTransaction().begin();
 		em.merge(site);
 		em.getTransaction().commit();
 		
 		return findAllSites();
 	}
+	
 	//deleteSite
-	public void removeSite(int id) {
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Site> removeSite(@PathParam("id") int id) {
 		em.getTransaction().begin();
 		Site site = em.find(Site.class, id);
 		em.remove(site);
 		em.getTransaction().commit();
+		return findAllSites();
 	}
 	
-
+	/*
+	 * Originally the assignment asked for a path api/site that lacked an id:
+	@DELETE
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Site> removeSite(int id) {
+		em.getTransaction().begin();
+		Site site = em.find(Site.class, id);
+		em.remove(site);
+		em.getTransaction().commit();
+		return findAllSites();
+	}
+	*/
 	
 	public static void main(String[] args) {
 		SiteDAO dao = new SiteDAO();
 		
-//		Site site = new Site(null, "Titanic", "Plot", "Poster", "SiteId");
-//		site = dao.createSite(site);
-//		System.out.println(site.getId());
-		
-//		Site titanic = dao.readSiteById(3);
-//		System.out.println(titanic.getTitle());
+//		Site newSite = new Site(3,"Site 3",12.23,23.34);
+//		newSite = dao.createSite(newSite);
+//		System.out.println(newSite.getId());
+//		
+		Site test = dao.findSite(3);
+//		System.out.println(test.getName());
 		
 //		dao.deleteSite(4);
+		
+//		test.setLatitude(100);
+//		dao.updateSite(3, test);
 		
 		List<Site> sites = dao.findAllSites();
 		for (Site site : sites) {
 			System.out.println(site.getName());
 		}
-		
-//		titanic.setPlot("Titanic sinks at the end");
-//		dao.updateSite(titanic);
-		
 	}
 
 }
